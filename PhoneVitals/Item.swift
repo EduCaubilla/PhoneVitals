4b8210b99ec271d587d9aed8ab19dde26bd0df8a
Edu Caubilla <educaubilla@gmail.com>
2025-10-01 16:43:51 +0200

    Initial Commit

---
+++ PhoneVitals/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PhoneVitals
+//
+//  Created by Edu Caubilla on 1/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PhoneVitalsTests/PhoneVitalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneVitalsTests.swift
+//  PhoneVitalsTests
+//
+//  Created by Edu Caubilla on 1/10/25.
+//
+
+import Testing
+@testable import PhoneVitals
+
+struct PhoneVitalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
